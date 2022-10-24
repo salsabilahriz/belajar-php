b827f37f31b7f9dcc85dfe34736b5b76398de515
Salsabilah Rizka Kamilah <salsabilahrizka83@gmail.com>
2022-10-24 12:18:10 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+echo "Hello World!<br>";
+
+$nama = "Salsabilah Rizka Kamilah";
+$umur = 21;
+
+echo "Nama saya <strong>$nama</strong>, saya berusia $umur tahun.<br>";
+
+$namaTeman= "Reza";
+$umurTeman= "23";
+
+echo "Nama teman saya <strong>$namaTeman</strong>, teman saya berusia $umurTeman tahun.<br>";
+
+$selisihUmur = $umur - $umurTeman;
+echo "Selisih usia saya dan teman saya adalah $selisihUmur tahun.";
+?>
